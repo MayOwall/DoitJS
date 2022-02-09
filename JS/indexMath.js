a0e8275490ebe14b5222b785da73f0c5fe1968ff
Owall <97934878+MayOwall@users.noreply.github.com>
2022-02-09 17:25:36 +0900

    Delete JS directory

--- JS/indexMath.js
+++
@@ -1,8 +0,0 @@
-document.write(`<br><div style="font-weight:900">==(Math methods)===========</div><br>`);
-
-document.write(`<b>당첨자를 랜덤추첨하는 프로그램 만들기</b><br>`);
-
-function randomNum(event) {
-  const winner = Math.floor((Math.random() * 50) + 1);
-  document.write(`당첨 번호는 "${winner}"`);
-};
